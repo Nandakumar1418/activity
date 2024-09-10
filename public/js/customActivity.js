b8f0443014ce0c722ee01aa3407bf3eced302e6d
Nandakumar <136629681+Nandakumar1418@users.noreply.github.com>
2024-09-10 22:04:46 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -50,18 +50,7 @@ define([
         $.each(inArguments, function (index, inArgument) {
             $.each(inArgument, function (key, val) {
 
-                if (key === 'accountSid') {
-                    $('#accountSID').val(val);
-                }
-
-                if (key === 'authToken') {
-                    $('#authToken').val(val);
-                }
-
-                if (key === 'messagingService') {
-                    $('#messagingService').val(val);
-                }
-
+               
                 if (key === 'body') {
                     $('#messageBody').val(val);
                 }                                                               
@@ -96,14 +85,8 @@ define([
         var body = $('#messageBody').val();
 
         payload['arguments'].execute.inArguments = [{
-            "accountSid": accountSid,
-            "authToken": authToken,
-            "messagingService": messagingService,
+           
             "body": body,
-            "firstName": "{{Contact.Attribute.Customers.FirstName}}",
-            "lastName": "{{Contact.Attribute.Customers.LastName}}",
-            "emailAddress": "{{Contact.Attribute.Customers.EmailAddress}}",
-            "phone": "{{Contact.Attribute.Customers.Phone}}",
             "to": "{{Contact.Attribute.Customers.Phone}}",
             "contactKey":"{{Contact.Attribute.Customers.ContactKey}}"
         }];
